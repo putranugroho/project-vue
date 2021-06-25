ebaa81b452d4d8dca2af536e2d2c0940d671068d
putra nugroho <putra.nugroho@alodokter.com>
2021-06-25 18:38:27 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,138 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div class="container"> -->
+    <!-- <Header text="Add Task" color="blue"/> -->
+  <!-- </div> -->
+  <!-- <div :class="[task.reminder ? 'reminder' : '', 'task']" @dblclick="$emit('delete-task', task.id)" @click="$emit('toggle-reminder', task.id)">
+        <h3>{{task.text}}</h3>
+        <p>{{task.day}}</p>
+    </div> -->
+    <div>
+      <form action="">
+        <div>
+          <label for="">Email</label>
+          <input type="email" placeholder="Email" v-model="email"/>
+        </div>
+        <div>
+          <label for="">Password</label>
+          <input type="password" placeholder="Password" v-model="password"/>
+        </div>
+        <button @click="login">Login</button>
+      </form>
+      <hr>
+      <div v-for="data in data">
+        <p>{{data.kode_gudang}}</p>
+        <p>{{data.nama_gudang}}</p>
+        <p>{{data.alamat_gudang}}</p>
+        <p>{{data.provinsi}}</p>
+        <p>{{data.kabupaten_kota}}</p>
+        <p>{{data.kecamatan}}</p>
+        <p>{{data.kelurahan}}</p>
+        <p>{{data.kodepos}}</p>
+        <p>{{data.deskripsi}}</p>
+        <hr>
+      </div>
+      <Input/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Input
+  },
+  data() {
+    return {
+      data: [],
+      token: String,
+      email: '',
+      password: ''
+    }
+  },
+  created(){
+    // this.data=[
+    //     {
+    //         "id_gudang": 1,
+    //         "kode_gudang": "GD01",
+    //         "nama_gudang": "gudanglagu.com",
+    //         "alamat_gudang": "Kuningan",
+    //         "provinsi": "DKI JAKARTA",
+    //         "kabupaten_kota": "JAKARTA SELATAN",
+    //         "kecamatan": "SETIABUDI",
+    //         "kelurahan": "KUNINGAN TIMUR",
+    //         "kodepos": 12950,
+    //         "deskripsi": "ahay",
+    //         "created_by": null,
+    //         "updated_by": null,
+    //         "status": 1
+    //     },
+    //     {
+    //         "id_gudang": 2,
+    //         "kode_gudang": "GD02",
+    //         "nama_gudang": "ISTANA JANUAR MEWAH BANGET",
+    //         "alamat_gudang": "DI INJEK",
+    //         "provinsi": "DKI JAKARTA",
+    //         "kabupaten_kota": "JAKARTA SELATAN",
+    //         "kecamatan": "SETIABUDI",
+    //         "kelurahan": "KUNINGAN TIMUR",
+    //         "kodepos": 12950,
+    //         "deskripsi": "ngipi",
+    //         "created_by": "3",
+    //         "updated_by": null,
+    //         "status": 1
+    //     },
+    //     {
+    //         "id_gudang": 3,
+    //         "kode_gudang": "GD03",
+    //         "nama_gudang": "ISTANA JANUAR SAMA SELINGKUHANNYA",
+    //         "alamat_gudang": "DI INJEK",
+    //         "provinsi": "DKI JAKARTA",
+    //         "kabupaten_kota": "JAKARTA SELATAN",
+    //         "kecamatan": "SETIABUDI",
+    //         "kelurahan": "KUNINGAN TIMUR",
+    //         "kodepos": 12950,
+    //         "deskripsi": "GASSSS",
+    //         "created_by": "3",
+    //         "updated_by": null,
+    //         "status": 1
+    //     },
+    //     {
+    //         "id_gudang": 4,
+    //         "kode_gudang": "GDG69",
+    //         "nama_gudang": "Gudang Garam",
+    //         "alamat_gudang": "Dressrosa",
+    //         "provinsi": "DKI JAKARTA",
+    //         "kabupaten_kota": "JAKARTA BARAT",
+    //         "kecamatan": "KEBON JERUK",
+    //         "kelurahan": "KEMANGGISAN",
+    //         "kodepos": null,
+    //         "deskripsi": "tess",
+    //         "created_by": "3",
+    //         "updated_by": null,
+    //         "status": 1
+    //     }
+    // ]
+  },
+  methods: {
+    login(){
+      const data = {
+        email: this.email,
+        password: this.password
+      }
+      
+      axios.post('https://api.big-solution.net/api/login', data).then(res=>this.token = res.token).catch(err=>console.log(err));
+      
+      const config = {
+          headers: { Authorization: `Bearer ${token}` }
+      };
+      
+      if (this.token) {
+        axios.get('https://api.big-solution.net/api/admin/warehouses', config).then(res=>this.data = res.data).catch(err=>console.log(err));
+      }
+    }
   }
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- <div :key="task.id" v-for="task in tasks">
+        <Task @toggle-reminder="$emit('toggle-reminder',task.id)" @delete-task="$emit('delete-task',task.id)" :task="task"/>
+    </div> -->
+      <form action="">
+        <div>
+          <label for="">kode_gudang</label>
+          <input type="text" placeholder="kode_gudang" v-model="kode_gudang"/>
+        </div>
+        <div>
+          <label for="">nama_gudang</label>
+          <input type="text" placeholder="nama_gudang" v-model="nama_gudang"/>
+        </div>
+        <div>
+          <label for="">alamat_gudang</label>
+          <input type="text" placeholder="alamat_gudang" v-model="alamat_gudang"/>
+        </div>
+        <div>
+          <label for="">provinsi</label>
+          <input type="text" placeholder="provinsi" v-model="provinsi"/>
+        </div>
+        <div>
+          <label for="">kabupaten_kota</label>
+          <input type="text" placeholder="kabupaten_kota" v-model="kabupaten_kota"/>
+        </div>
+        <div>
+          <label for="">kecamatan</label>
+          <input type="text" placeholder="kecamatan" v-model="kecamatan"/>
+        </div>
+        <div>
+          <label for="">kelurahan</label>
+          <input type="text" placeholder="kelurahan" v-model="kelurahan"/>
+        </div>
+        <div>
+          <label for="">kodepos</label>
+          <input type="text" placeholder="kodepos" v-model="kodepos"/>
+        </div>
+        <button @click="Input">Login</button>
+      </form>
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    data(){
+        return {
+            kode_gudang: '',
+            nama_gudang: '',
+            alamat_gudang: '',
+            provinsi: '',
+            kabupaten_kota: '',
+            kecamatan: '',
+            kelurahan: '',
+            kodepos: ''
+        }
+    },
+  methods: {
+    Input(){
+      const data = {
+        kode_gudang: this.kode_gudang,
+        nama_gudang: this.nama_gudang,
+        alamat_gudang: this.alamat_gudang,
+        provinsi: this.provinsi,
+        kabupaten_kota: this.kabupaten_kota,
+        kecamatan: this.kecamatan,
+        kelurahan: this.kelurahan,
+        kodepos: this.kodepos
+      }
+      
+      axios.post('https://api.big-solution.net/api/login', data).then(res=>this.token = res.token).catch(err=>console.log(err));
+    }
+  }
+}
+</script>
